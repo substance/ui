91e8a4fbf44e1b455e496a1d2bb1570e5ce5c0b1
Michael Aufreiter <michael@substance.io>
2015-06-03 18:40:06 +0200

    Initial set of commonly used ui components (extracted from Substance Journal)

---
+++ writer/tools/index.js
@@ -0,0 +1,11 @@
+var TextTool = require("./text_tool");
+var UndoTool = require("./undo_tool");
+var RedoTool = require("./redo_tool");
+var SaveTool = require("./save_tool");
+
+module.exports = [
+  TextTool,
+  UndoTool,
+  RedoTool,
+  SaveTool
+];
